513e4cc2d0ac8e7f874d3c0eb553268023672ab5
lai2-01 <lai2-01@itismeucci.com>
2022-01-12 13:41:13 +0100

    init

---
+++ demo/src/main/java/com/example/classe.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class classe 
+{ 
+    public static void main(String[] args) throws ParserConfigurationException, SAXException 
+    { 
+        try {
+            File file = new File("classe.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(file);
+            document.getDocumentElement().normalize();
+            System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
+            NodeList nList = document.getElementsByTagName("studenti");
+            System.out.println("----------------------------");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                System.out.println("\nCurrent Element :" + nNode.getNodeName());
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("studenti id : " + eElement.getAttribute("id"));
+                    System.out.println("nome : " + eElement.getElementsByTagName("nome").item(0).getTextContent());
+                    System.out.println("cognome : " + eElement.getElementsByTagName("cognome").item(0).getTextContent());
+                    System.out.println("annoDiNascità : " + eElement.getElementsByTagName("annoDiNascità").item(0).getTextContent());
+                }
+            }
+        }
+        catch(IOException e) {
+            System.out.println(e);
+        } 
+    }
+}
